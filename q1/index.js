e4753f00fe821acb7a4a9d216b6163d4b447499b
AbenezerM <abenezermulugeta41@gmail.com>
2022-09-25 18:23:07 -0500

    Add age query parameter

--- q1/index.js
+++ q1/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   let name = req.query.name;
+  let age = req.query.age;
+
   if (!name) {
     name = "person";
   }
-  res.send(`Welcome ${name}`);
+
+  res.send(`Welcome ${name}, ${age}`);
 });
 
 app.listen(3000);
